60ffe6e4401e1b9eb59674175e31ccb1ba21ba6b
abhishek.naik <abhinaik1432@gmail.com>
2025-10-11 09:44:47 +0530

    Assignment no 1_Task 1.py

---
+++ Assignment no 1_Task 2.py
@@ -0,0 +1,11 @@
+#Task 2: Create a Personalized Greeting
+#Problem Statement: Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+first_name=input(str("enter a first name: "))
+last_name=input(str("enter a last name: "))
+#2.  Concatenates the first name and last name into a full name.
+full_name=first_name+" "+last_name
+#3.  Print a personalized greeting message using the full name.
+print('hello,',full_name+'! welcome to python programming')
+#Expected Output:
+#The program should output a greeting like:
