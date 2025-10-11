065de431a1a03b909313142e86008bf9f1b88f8d
abhishek.naik <abhinaik1432@gmail.com>
2025-10-11 08:22:08 +0530

    Assignment no 1_Task 1.py and Assignment no 1_Task 2.py

---
+++ Assignment no 1_Task 1.py
@@ -0,0 +1,8 @@
+#Task 1: Perform Basic Mathematical Operations
+#1.  Takes two numbers as input from the user.
+user= int(input("Enter a first number: "))
+user_2= int(input("Enter a second number: "))
+print("Addition: ",user+user_2)
+print("Substraction: ",user-user_2)
+print("Multiplication: ",user*user_2)
+print("Division: ",user/user_2)
